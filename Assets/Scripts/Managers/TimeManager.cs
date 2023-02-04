8c9155e05997df8055a45b78f329ad5b8d3834a6
Randres <andplayer230@gmail.com>
2023-02-03 18:00:59 -0600

    ADD Text Mesh Pro y controles basicos de manejo de tiempo

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    [Header("Actual Year")]
     public float actualYear = 2000;
+
+    [Header("Add times to the time")]
     public float addTime = 0;
+    public float addToTime = 1;
+    public bool addOrLessTime = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,11 +21,33 @@ public class TimeManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (addOrLessTime)
+            AddOrLessTime(addToTime);
+
         actualYear += addTime * Time.fixedDeltaTime;
     }
 
-    public void AddOrLessTime(float timeYear)
+    private void AddOrLessTime(float timeYear)
+    {
+        addTime += timeYear * Time.fixedDeltaTime;
+    }
+    public void SetAddTime(float timeYear)
+    {
+        addToTime = timeYear;
+    }
+
+    public void PauseOrPlaySetAddTime(float timeYear)
     {
         addTime = timeYear;
+    }
+
+    public void OnMouseDown()
+    {
+        addOrLessTime = true;
+    }
+
+    public void OnMouseUp()
+    {
+        addOrLessTime = false;
     }
 }
